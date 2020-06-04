67836b675ad9399e2cdc7489a8d90054cd73f013
moitra-ayon <ayonmoitra1911@gmail.com>
2020-06-04 19:32:04 +0530

    Concept of iterator interface added to HashSet.

--- Collection, Generics and Stream API/src/MyHashSet.java
+++ Collection, Generics and Stream API/src/MyHashSet.java
@@ -3,22 +3,28 @@ import java.util.*;
 public class MyHashSet {
 
 	public static void main(String[] args) {
-		
+
 		Set set = new HashSet();
-		
+
 		set.add("Zeroth");
-		
+
 		set.add("First");
-		
+
 		set.add("Second");
-		
+
 		set.add(3);
-		
+
 		set.add(4);
-		
+
 		set.add(4);
-		
-		System.out.println(set);
+
+//		System.out.println(set); instead use iterator
+
+		Iterator iterator = set.iterator();
+
+		while (iterator.hasNext()) {
+			System.out.println(iterator.next());
+		}
 
 	}
 
