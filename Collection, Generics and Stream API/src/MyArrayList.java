5c29c17664f4d61557d7bc8fbcff0591f7f38787
moitra-ayon <ayonmoitra1911@gmail.com>
2020-06-04 10:34:00 +0530

    Few classes have been added and the implementation of ArrayList has been achieved.

---
+++ Collection, Generics and Stream API/src/MyArrayList.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class MyArrayList {
+
+	public static void main(String[] args) {
+
+		List list = new ArrayList();
+
+		list.add("Zeroth");
+		list.add("First");
+		list.add("2nd");
+		list.add("Third");
+//		list.add(new Float(21.12f));
+		list.add(21.12f);
+		System.out.println(list);
+	}
+
+}
---
+++ Collection, Generics and Stream API/src/MyHashSet.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class MyHashSet {
+
+	public static void main(String[] args) {
+
+	}
+
+}
