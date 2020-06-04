9bc8a68a4c721765e11813138018cd33a82600ae
moitra-ayon <ayonmoitra1911@gmail.com>
2020-06-04 18:55:57 +0530

    Use of iterator interface.

--- Collection, Generics and Stream API/src/MyArrayList.java
+++ Collection, Generics and Stream API/src/MyArrayList.java
@@ -7,12 +7,24 @@ public class MyArrayList {
 		List list = new ArrayList();
 
 		list.add("Zeroth");
+		
 		list.add("First");
+		
 		list.add("2nd");
+		
 		list.add("Third");
-//		list.add(new Float(21.12f));
+		
+//		list.add(new Float(21.12f)); deprecated way
 		list.add(21.12f);
-		System.out.println(list);
+		
+//		System.out.println(list); instead use iterator
+		
+		Iterator iterator = list.iterator();
+		
+		while(iterator.hasNext())
+		{
+			System.out.println(iterator.next());
+		}
 	}
 
 }
