e2fdd928c9240adc0501a951b919efaf76251328
moitra-ayon <ayonmoitra1911@gmail.com>
2020-06-04 19:37:30 +0530

    Implementation of HashMap added with iterator interface.

--- Collection, Generics and Stream API/src/MyHashMap.java
+++ Collection, Generics and Stream API/src/MyHashMap.java
@@ -1,8 +1,34 @@
+
+//Non-generic 
 import java.util.*;
 
 public class MyHashMap {
 
 	public static void main(String[] args) {
+
+		Map map = new HashMap();
+
+		// Adding elements to map
+
+		map.put(1, "Sachin");
+
+		map.put(5, "Suresh");
+
+		map.put(2, "Gautam");
+
+		map.put(6, "Rohit");
+
+		// Traversing Map
+
+		// Converting to Set so that we can traverse
+		Set set = map.entrySet();
+
+		Iterator itr = set.iterator();
+		while (itr.hasNext()) {
+
+			Map.Entry entry = (Map.Entry) itr.next();
+			System.out.println(entry.getKey() + " " + entry.getValue());
+		}
 
 	}
 
