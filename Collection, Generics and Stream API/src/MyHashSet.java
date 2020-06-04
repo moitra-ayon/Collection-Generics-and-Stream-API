05e76b65601618585978c402fa99899ff44f40f8
moitra-ayon <ayonmoitra1911@gmail.com>
2020-06-04 18:59:48 +0530

    Implementation of HashSet.

--- Collection, Generics and Stream API/src/MyHashSet.java
+++ Collection, Generics and Stream API/src/MyHashSet.java
@@ -3,6 +3,14 @@ import java.util.*;
 public class MyHashSet {
 
 	public static void main(String[] args) {
+		
+		Set set = new HashSet();
+		
+		set.add("Zeroth");
+		set.add("First");
+		set.add("Second");
+		set.add(3);
+		set.add(4);
 
 	}
 
